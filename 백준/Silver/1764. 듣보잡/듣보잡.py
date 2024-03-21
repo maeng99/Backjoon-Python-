9e05d49a40f87bf72795f623802390a6b9437d4a
maeng99 <137726145+maeng99@users.noreply.github.com>
2024-03-21 11:24:09 +0900

    [Silver IV] Title: 듣보잡, Time: 176 ms, Memory: 126440 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,19 @@
+import sys
+
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+nl=[input().rstrip() for _ in range(n)]
+ml=[input().rstrip() for _ in range(m)]
+dict={}
+dict2={}
+
+for i in nl:
+  dict[i]=1
+for i in ml:
+  if i in dict:
+    dict2[i]=1
+dict2=sorted(dict2.keys())
+print(len(dict2))
+for i in dict2:
+  print(i)
